477bca28d729a82a44377ec628ff2150a5bd28fc
Siddharth <siddharth@vasantsoftwaressolution.com>
2024-07-31 18:51:50 +0530

    make smooth transition and sound effects

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,11 +3,13 @@ import bg from '../../assets/bg.mp4';
 import logo from '../../assets/HANUMAN.png';
 import btn from '../../assets/btn.png';
 import frame from '../../assets/frame.png';
-import { useEffect, useState } from 'react';
+import voice from '../../assets/cinematic-suspense.mp3';
+import click from '../../assets/click_button.ogg';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 const Home = () => {
 
-const navigate =  useNavigate();
+  const navigate = useNavigate();
   const [size, setsize] = useState({
     height: window.innerHeight,
     width: window.innerWidth
@@ -16,19 +18,37 @@ const navigate =  useNavigate();
   const [opacity1, setopacity1] = useState(0)
   const [opacity2, setopacity2] = useState(0)
   const [opacity3, setopacity3] = useState(0)
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
 
   setTimeout(() => {
     setopacity(1);
     setTimeout(() => {
-    setopacity1(1);
+      setopacity1(1);
       setTimeout(() => {
-      setopacity2(1);
+        setopacity2(1);
         setTimeout(() => {
           setopacity3(1);
         }, 5000);
       }, 3000);
     }, 2000);
   }, 3000);
+  const hommey = () =>{
+    const audio = new Audio(click);
+    audio.play();
+    setTimeout(() => {
+      navigate('/about')
+    }, 2000);
+  }
+
+  useEffect(() => {
+
+   return () =>{
+     audioRef.current?.play();
+   }
+  });
+
+
 
   useEffect(() => {
     setsize({
@@ -38,15 +58,17 @@ const navigate =  useNavigate();
   }, [onresize])
   return (
     <>
+
       <div className="loader h-screen w-full fixed top-0 left-0 z-30 bg-[#141414] p-[3vw]">
+      <audio onEnded={()=>{audioRef.current?.play()}} ref={audioRef} src={voice} />
         <div className="relative size-full flex flex-col items-center justify-center gap-[3vw]">
           <img style={{ opacity: opacity1 }} className=" transition-opacity loader-border absolute h-full w-full top-0 left-0" src={frame} alt="" />
           <img style={{ opacity: opacity2 }} className='transition-opacity logo w-[25vw] -translate-y-[10%]' src={logo} alt="" />
           <button className="enter-experience-btn relative px-[2.5vw] transition-opacity py-[1vw] max-2xl:text-xs text-[#141414] uppercase translate-y-[0px] translate-x-[0px]" style={{ translate: 'none', rotate: 'none', scale: 'none', opacity: opacity3 }}
-          onClick={() => navigate('/about')}
+            onClick={() => hommey()}
           >
             Enter the experience
-            <img  className="size-full absolute top-0 left-0 -z-[8]" src={btn} alt="" />
+            <img className="size-full absolute top-0 left-0 -z-[8]" src={btn} alt="" />
           </button>
         </div>
         <video style={{ opacity: opacity }} src={bg} autoPlay muted loop height={size.height} width={size.width} className='transition-opacity overlay-video size-full absolute top-0 left-0 object-cover -z-10'></video>
