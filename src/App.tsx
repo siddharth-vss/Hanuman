3cd64d25da52d19a802e1afecc0c8d77a130c0d9
Siddharth <siddharth@vasantsoftwaressolution.com>
2024-07-31 18:50:36 +0530

    add someitems to make auto matic sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import Home from "./pages/home"
 import About from "./pages/about"
-import {BrowserRouter ,Routes , Route} from 'react-router-dom' ;
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 const App = () => {
+
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Demo />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-    </Routes>
+      </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
+
+
+const Demo = () => {
+  const navigate = useNavigate();
+  const click = () => {
+
+    navigate("/home");
+  }
+  return <><h1 onClick={click} className="bg-black text-white cursor-pointer" >CLICK ON ME</h1></>
+}
