feb36e5e694a781b3cbde19e13bf01521f977fb2
Siddharth <siddharth@vasantsoftwaressolution.com>
2024-07-31 18:51:04 +0530

    make nav bar ui

--- src/component/nav/index.tsx
+++ src/component/nav/index.tsx
@@ -1,10 +1,36 @@
+import { useNavigate } from "react-router-dom"
+import img from '../../assets/HANUMAN.png';
+import './nav.css';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  
+
   return (
     <>
-      <div className="h-[5vh] sticky top-0 w-screen bg-[#FFFFFF99] border-b border-zinc-500 backdrop-blur-md">
-        hello
-      </div>
+      <nav><a><img src={img} alt="" className="h-[30px]" onClick={()=>{navigate('/')}} /></a>
+      <input type="checkbox" id="hamburger" />
+      <label htmlFor="hamburger">
+        <i className="fa-solid fa-bars"></i>
+      </label>
+      <ul>
+        <li>
+          <a onClick={()=>{navigate('/')}} className="active">Home</a>
+        </li>
+        <li>
+          <a onClick={()=>{navigate('/')}}>About</a>
+        </li>
+        <li>
+          <a onClick={()=>{navigate('/')}}>Features</a>
+        </li>
+        <li>
+          <a onClick={()=>{navigate('/')}}>services</a>
+        </li>
+        <li>
+          <a onClick={()=>{navigate('/')}}>F.A.Q</a>
+        </li>
+      </ul>
+    </nav>
     </>
   )
 }
